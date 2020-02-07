1004b79126562d8626bf511564c35208c0998e7c
Vavarooks <vavarooks123@gmail.com>
2020-02-07 12:49:24 -0800

    readme add, indext and Html edits

---
+++ README.md
@@ -0,0 +1,14 @@
+Where to assess:
+
+https://github.com/Vavarooks/Homework6
+
+https://vavarooks.github.io/Homework6/index.html
+
+
+Contains: HTML, Javascript, and Css
+
+HTML: Holds the main bare bones of the code. H1, H2, H3, P, Div, and Form tags.
+
+Css: COntains the fixes and coloring for the page.
+
+Javascript: Holds the code that gets the users location and the location they search for to inform them of the weather.
--- javascript/back.js
+++ javascript/back.js
@@ -1,1 +1,42 @@
-README.md
+$(document).ready(function(){
+    // Get value on button click and show alert
+    $("#look").click(function(){
+        var userInput = $("#inputState").val();
+        console.log(userInput)
+    });
+});
+var APIKey = "166a433c57516f51dfab1f7edaed8413"
+
+var queryURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901"+ userInput + "&appid=" + APIKey;
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+
+    console.log(response)
+
+    console.log(response.sys.country);
+
+    console.log(response.wind);
+
+    console.log(response.main.humidity);
+
+    console.log(response.main.temp);
+
+    const cityEl = $(`.city`).text(`City Name: ${response.name}`);
+
+      const windEl = $(`.wind`).text(`Wind Speed: ${response.wind.speed}`);
+
+      const weatherEl = $(`.humidity`).text(`Humidity: ${response.main.humidity}`);
+
+      const heatEl = $(`.temp`).text(`Temputure: ${response.main.temp}`)
+
+      F = (K - 273.15) * 1.80 + 32;
+
+      
+  console.log(cityEl);
+  console.log(windEl);
+  console.log(weatherEl);
+  console.log(heatEl);
+  });
