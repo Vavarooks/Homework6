590778b0514a410b014688e75e6674b0236f00d0
Vavarooks <vavarooks123@gmail.com>
2020-02-13 15:00:03 -0800

    Css, HTML and Javasript edit

--- javascript/back.js
+++ javascript/back.js
@@ -1,51 +1,45 @@
 
-$(".look").on("click", function(event){
+$("#addon-wrapping").on("click", function(event){
   event.preventDefault();
   
-  var weatherInt = $(".inputGroupSelect01").val();
-  var stateInt =  $(".inputState").text();
-  var cityInt = $(".inputCity").text();
+  
+  var cityInt = $("#inputCity").val();
 
-  console.log(stateInt);
   console.log(cityInt)
   
-  localStorage.setItem(stateInt, cityInt, weatherInt );
+  // localStorage.setItem( cityInt);
   
+  var APIKey = "166a433c57516f51dfab1f7edaed8413"
+  
+  var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityInt}&appid=`+ APIKey;
+  
+  console.log(queryURL);
+  
+  $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+
+$("#cityName").append(cityInt);
+
+for (let i = 0 ; i < response.list.length ; i = i + 8){
+
+  console.log(response.list[i]);
+
+$(".weatherResult").append(`<div class="weatherStuff card" style="width: 25rem;" > <h3> Date ${response.list[i].dt_txt.split(" ")[0]} </h3> <h3> The temputure ${response.list[i].main.temp}  </h3> 
+<h3>  ${response.list[i].weather[0].main} </h3> <img src= "https://openweathermap.org/img/wn/${response.list[i].weather[0].icon}.png" class="card-img-top" >
+<h3> The wind speeed ${response.list[i].wind.speed} Mph </h3> <h3> The humidity ${response.list[i].main.humidity} % </h3> </div>`)
+
+
+}
+
+  
+        
+    console.log(cityEl);
+    console.log(windEl);
+    console.log(weatherEl);
+    console.log(heatEl);
+    });
   });
 
 
-var APIKey = "166a433c57516f51dfab1f7edaed8413"
-
-var queryURL = `api.openweathermap.org/data/2.5/forecast/daily?q= ${cityInt}, &cnt= ${weatherInt} &appid=` + APIKey;
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function (response) {
-
-    console.log(response)
-
-    console.log(response.sys.country);
-
-    console.log(response.wind);
-
-    console.log(response.main.humidity);
-
-    console.log(response.main.temp);
-
-    const cityEl = $(`.city`).text(`City Name: ${response.name}`);
-
-      const windEl = $(`.wind`).text(`Wind Speed: ${response.wind.speed}`);
-
-      const weatherEl = $(`.humidity`).text(`Humidity: ${response.main.humidity}`);
-
-      const heatEl = $(`.temp`).text(`Temputure: ${response.main.temp}`)
-
-      F = (K - 273.15) * 1.80 + 32;
-
-      
-  console.log(cityEl);
-  console.log(windEl);
-  console.log(weatherEl);
-  console.log(heatEl);
-  });
