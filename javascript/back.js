9364d6fc6fed1380e9620bc0b30785e073cb219e
Vavarooks <vavarooks123@gmail.com>
2020-02-14 13:06:57 -0800

    Minor javascript edits

--- javascript/back.js
+++ javascript/back.js
@@ -26,7 +26,7 @@ for (let i = 0 ; i < response.list.length ; i = i + 8){
 
   console.log(response.list[i]);
 
-$(".weatherResult").append(`<div class="weatherStuff card" style="width: 25rem;" > <h3> Date ${response.list[i].dt_txt.split(" ")[0]} </h3> <h3> The temputure ${response.list[i].main.temp}  </h3> 
+$(".weatherResult").append(` <div class="weatherStuff card" style="width: 25rem;" > <h3> Date ${response.list[i].dt_txt.split(" ")[0]} </h3> <h3> The temputure ${response.list[i].main.temp}  </h3> 
 <h3>  ${response.list[i].weather[0].main} </h3> <img src= "https://openweathermap.org/img/wn/${response.list[i].weather[0].icon}.png" class="card-img-top" >
 <h3> The wind speeed ${response.list[i].wind.speed} Mph </h3> <h3> The humidity ${response.list[i].main.humidity} % </h3> </div>`)
 
@@ -42,4 +42,14 @@ $(".weatherResult").append(`<div class="weatherStuff card" style="width: 25rem;"
     });
   });
 
+  var currentW = `http://api.openweathermap.org/data/2.5/weather?q=" + ${searchValue} + "&appid=7ba67ac190f85fdba2e2dc6b9d32e93c&units=imperial`
 
+
+  var uvInex = ` "http://api.openweathermap.org/data/2.5/uvi?appid=7ba67ac190f85fdba2e2dc6b9d32e93c&lat=" + ${lat} + "&lon=" + ${lon} `
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+console.log(response)
+  })
