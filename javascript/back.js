de30b624f16eea351805c8f932eb854feea5ec56
Vavarooks <vavarooks123@gmail.com>
2020-02-15 10:23:13 -0800

    Fixed Temputure

--- javascript/back.js
+++ javascript/back.js
@@ -1,11 +1,13 @@
-
 $("#addon-wrapping").on("click", function(event){
   event.preventDefault();
   
-  
   var cityInt = $("#inputCity").val();
+  city = $(`#inputCity`).text();
 
-  console.log(cityInt)
+  console.log(cityInt);
+  console.log(city);
+
+  localStorage.setItem(cityInt, city);  
   
   // localStorage.setItem( cityInt);
   
@@ -26,30 +28,35 @@ for (let i = 0 ; i < response.list.length ; i = i + 8){
 
   console.log(response.list[i]);
 
-$(".weatherResult").append(` <div class="weatherStuff card" style="width: 25rem;" > <h3> Date ${response.list[i].dt_txt.split(" ")[0]} </h3> <h3> The temputure ${response.list[i].main.temp}  </h3> 
+  var tempF = (response.list[i].main.temp - 273.15) * 1.80 + 32 ;
+  $(".tempF").text("Temperature (Kelvin) " + tempF); 
+
+$(".weatherResult").append(` <div class="weatherStuff card" style="width: 25rem;" > <h3> Date ${response.list[i].dt_txt.split(" ")[0]} </h3> <h3 class="tempF"> The temputure ${response.list[i].main.temp} </h3> 
 <h3>  ${response.list[i].weather[0].main} </h3> <img src= "https://openweathermap.org/img/wn/${response.list[i].weather[0].icon}.png" class="card-img-top" >
 <h3> The wind speeed ${response.list[i].wind.speed} Mph </h3> <h3> The humidity ${response.list[i].main.humidity} % </h3> </div>`)
 
+}    
 
-}
 
-  
-        
-    console.log(cityEl);
-    console.log(windEl);
-    console.log(weatherEl);
-    console.log(heatEl);
     });
+    
+    
+    var pastSearch = cityInt;
+  
+    var data = localStorage.getItem(pastSearch);
+   
+   $("#pastSearch").append(data);
   });
+  
+  
 
-  var currentW = `http://api.openweathermap.org/data/2.5/weather?q=" + ${searchValue} + "&appid=7ba67ac190f85fdba2e2dc6b9d32e93c&units=imperial`
+  // var currentW = `http://api.openweathermap.org/data/2.5/weather?q=" + ${searchValue} + "&appid=7ba67ac190f85fdba2e2dc6b9d32e93c&units=imperial`
 
+  // var uvInex = ` "http://api.openweathermap.org/data/2.5/uvi?appid=7ba67ac190f85fdba2e2dc6b9d32e93c&lat=" + ${lat} + "&lon=" + ${lon} `
 
-  var uvInex = ` "http://api.openweathermap.org/data/2.5/uvi?appid=7ba67ac190f85fdba2e2dc6b9d32e93c&lat=" + ${lat} + "&lon=" + ${lon} `
-
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function (response) {
-console.log(response)
-  })
+//   $.ajax({
+//     url: queryURL,
+//     method: "GET"
+//   }).then(function (response) {
+// console.log(response)
+//   })
