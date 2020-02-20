865413e7915d18d65aad3eff1e18b7074ef660e0
Vavarooks <vavarooks123@gmail.com>
2020-02-20 15:03:08 -0800

    Html fixed and Java edits

--- javascript/back.js
+++ javascript/back.js
@@ -1,70 +1,123 @@
-var cityEl = JSON.parse(localStorage.getItem("data"));
+var cityEl = JSON.parse(localStorage.getItem("weatherSearch"))||[];
 
-$("#addon-wrapping").on("click", function(event){
+if (cityEl.length > 0 ){
+  $("#pastCity").append(`<h6> Previous Search </h6> <ul>`)
+  for (let i = 0 ; i < cityEl.length ; i ++){
+    $("#pastCity").append(`<button class="pastSearch"> ${cityEl[i]} </button>`)
+  }
+  $("#pastCity").append(`</ul>`)
+}
+
+var APIKey = "166a433c57516f51dfab1f7edaed8413";
+
+$("#addon-wrapping").on("click", function (event) {
   event.preventDefault();
 
-  
+
   var cityInt = $("#inputCity").val();
   city = $(`#inputCity`).text();
 
   console.log(cityInt);
   console.log(city);
 
-  const cityShow = {
-    cityInt,
-    city,
-  }
+  cityEl.push(cityInt)
 
-  localStorage.setItem(`data`, JSON.stringify(cityShow));
+  localStorage.setItem(`weatherSearch`, JSON.stringify(cityEl));
 
-  // localStorage.setItem( cityInt);
-  
-  var APIKey = "166a433c57516f51dfab1f7edaed8413"
-  
-  var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityInt}&appid=`+ APIKey;
-  
+  var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityInt}&appid=` + APIKey + `&units=imperial`;
+
   console.log(queryURL);
-  
+
+  currentWeather(cityInt);
+
   $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function (response) {
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
 
-$("#cityName").append(cityInt);
+    $("#cityName").append(cityInt);
 
-for (let i = 0 ; i < response.list.length ; i = i + 8){
+    for (let i = 0; i < response.list.length; i = i + 8) {
 
-  console.log(response.list[i]);
+      console.log(response.list[i]);
 
-  var tempF = (response.list[i].main.temp - 273.15) * 1.80 + 32 ;
-  $(".tempF").text("Temperature (Kelvin) " + tempF); 
-
-$(".weatherResult").append(` <div class="weatherStuff card" style="width: 25rem;" > <h3> Date ${response.list[i].dt_txt.split(" ")[0]} </h3> <h3 class="tempF"> The temputure ${response.list[i].main.temp} </h3> 
+      $(".weatherResult").append(` <div class="weatherStuff card" style="width: 25rem;" > <h3> Date ${response.list[i].dt_txt.split(" ")[0]} </h3> <h3> The temputure ${response.list[i].main.temp} </h3> 
 <h3>  ${response.list[i].weather[0].main} </h3> <img src= "https://openweathermap.org/img/wn/${response.list[i].weather[0].icon}.png" class="card-img-top" >
 <h3> The wind speeed ${response.list[i].wind.speed} Mph </h3> <h3> The humidity ${response.list[i].main.humidity} % </h3> </div>`)
 
-}    
+    }
 
 
-    });
-    
-    
-    var pastSearch = cityInt;
-  
-    var data = localStorage.getItem(pastSearch);
-   
-   $("#pastSearch").append(data);
   });
+
+
+  // var pastSearch = cityInt;
+
+  // var data = localStorage.getItem(pastSearch);
+
+  // $("#pastSearch").append(data);
+});
+
+function currentWeather(searchValue) {
+
+  var currentW = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=${APIKey}&units=imperial`
+ 
+  $.ajax({
+    url: currentW,
+    method: "GET"
+  }).then(function (response) {
+
+    console.log(response)
+    var uvInex = `https://api.openweathermap.org/data/2.5/uvi?appid=${APIKey}&lat=${response.coord.lat}&lon=${response.coord.lon}`
+    $.ajax({
+      url: uvInex,
+      method: "GET"
+    }).then(function (result) {
+      console.log(result)
+
+$("#currentW").append(`<div class="card"> <p>${response.name}</p>
+ <p>${response.main.temp}</p> <p>${response.weather[0].description}</p>
+ <img src= "https://openweathermap.org/img/wn/${response.weather[0].icon}.png" class="card-img-top"> 
+ <p> ${result.value} </p>`)
+
+
+    })
+
+  })
+
+}
+
+$("#pastCity").on('click', '.pastSearch', function(event){
+  event.preventDefault();
+
+  var cityInt = $(this).text();
+  console.log (cityInt);
   
-  
+  var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityInt}&appid=` + APIKey + `&units=imperial`;
 
-  // var currentW = `http://api.openweathermap.org/data/2.5/weather?q=" + ${searchValue} + "&appid=7ba67ac190f85fdba2e2dc6b9d32e93c&units=imperial`
+  console.log(queryURL);
 
-  // var uvInex = ` "http://api.openweathermap.org/data/2.5/uvi?appid=7ba67ac190f85fdba2e2dc6b9d32e93c&lat=" + ${lat} + "&lon=" + ${lon} `
+  currentWeather(cityInt);
 
-//   $.ajax({
-//     url: queryURL,
-//     method: "GET"
-//   }).then(function (response) {
-// console.log(response)
-//   })
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+
+    $("#cityName").append(cityInt);
+
+    for (let i = 0; i < response.list.length; i = i + 8) {
+
+      console.log(response.list[i]);
+
+      $(".weatherResult").append(` <div class="weatherStuff card" style="width: 25rem;" > <h3> Date ${response.list[i].dt_txt.split(" ")[0]} </h3> <h3> The temputure ${response.list[i].main.temp} </h3> 
+<h3>  ${response.list[i].weather[0].main} </h3> <img src= "https://openweathermap.org/img/wn/${response.list[i].weather[0].icon}.png" class="card-img-top" >
+<h3> The wind speeed ${response.list[i].wind.speed} Mph </h3> <h3> The humidity ${response.list[i].main.humidity} % </h3> </div>`)
+
+    }
+
+
+  });
+
+
+})
